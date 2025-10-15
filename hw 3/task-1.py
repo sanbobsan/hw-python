b8b16e544bfa30064b09b93a05728e8aa349f21e
bob <saintsanbob@gmail.com>
2025-10-15 21:22:52 +0800

    update hw 3

--- hw 3/task-1.py
+++ hw 3/task-1.py
@@ -1,2 +1,2 @@
 
-nums = list(map(int, input(">> ").split())); print(*[i for i in set(nums) if nums.count(i) is not 1])
+nums = list(map(int, input(">> ").split())); print(*[i for i in set(nums) if nums.count(i) != 1])
